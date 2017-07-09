5f1df7c3ecf2110ff2c3e43e5665d11bfe4c56ca
KIM\joo <qweasd_147@naver.com>
2017-07-10 08:55:17 +0900

    로긴 하려는중

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import { Row, Col, Button } from 'antd';
+
 class Login extends React.Component {
 
     constructor(props) {
@@ -15,7 +17,14 @@ class Login extends React.Component {
         
         return (
             <div>
-                <h1>Login</h1>
+                <Row type="flex" justify="center">
+                    <Col span={8}>
+                        
+                            <Button type="primary" style={{ width: '300', display : "block", textAlign : "center" }}>Primary</Button>
+                        
+                        <Button type="primary">Primary</Button>
+                    </Col>
+                </Row>
             </div>
         );
     }
